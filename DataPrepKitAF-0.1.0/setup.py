1009201d06567194a95e4e308d09abe980b62826
AfEtaiwi02 <154633035+AfEtaiwi02@users.noreply.github.com>
2024-03-22 22:34:19 +0300

    Add files via upload

---
+++ DataPrepKitAF-0.1.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+setuptools.setup(
+    name = "DataPrepKit",
+    version = "0.1.0",
+    author = "Abdulaziz Fahad",
+    description= "Data preperation package",
+    packages = setuptools.find_packages(),
+    classifiers= ["Programming Language :: Python :: 3",
+                  "Operating System :: OS Independent",
+                  "License :: OSI Approved :: MIT License"]
+)
